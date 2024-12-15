64b84d185ce804f96ad23695327896c1be0b0825
Rafael Cavalcante <rvmascavalcante@gmail.com>
2024-12-15 20:20:53 -0300

    feat: add actions workflow and terraform iac

---
+++ main.tf
@@ -0,0 +1,20 @@
+data "aws_iam_role" "lab_role" {
+  name = "LabRole"
+}
+
+resource "aws_lambda_function" "authorizer" {
+  function_name = "cognito-authorizer"
+  role          = data.aws_iam_role.lab_role.arn
+  handler       = "src/index.handler"
+  runtime       = "nodejs20.x"
+  filename      = "lambda.zip"
+  publish       = true
+  
+  environment {
+    variables = {
+      USER_POOL_ID = "us-east-1_examplePoolId"
+      REGION       = "us-east-1"
+    }
+  }
+}
+
