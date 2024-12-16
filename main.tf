ec44395cd1ca472566fe5256dec58d3adb9ba551
Rafael Cavalcante <rvmascavalcante@gmail.com>
2024-12-15 22:07:28 -0300

    feat: add lambda function authorizer iac

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ data "aws_iam_role" "lab_role" {
   name = "LabRole"
 }
 
+data "aws_apigatewayv2_api" "api_gateway" {
+  api_id = var.api_gateway_id
+}
+
 resource "aws_lambda_function" "authorizer" {
   function_name    = "cognito-authorizer"
   role             = data.aws_iam_role.lab_role.arn
@@ -17,4 +21,13 @@ resource "aws_lambda_function" "authorizer" {
     variables = {
     }
   }
+}
+
+resource "aws_apigatewayv2_authorizer" "lambda_authorizer" {
+  name                              = "lambda-authorizer"
+  api_id                            = data.aws_apigatewayv2_api.api_gateway.id
+  authorizer_type                   = "REQUEST"
+  authorizer_uri                    = aws_lambda_function.authorizer.invoke_arn
+  identity_sources                  = ["$request.header.Authorization"]
+  authorizer_payload_format_version = "2.0"
 }
