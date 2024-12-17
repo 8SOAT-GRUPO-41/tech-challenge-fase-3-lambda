f066409337e0b65856e570d79be285b0cfce5664
Rafael Cavalcante <rvmascavalcante@gmail.com>
2024-12-17 01:52:32 -0300

    feat: add docs route proxy

--- main.tf
+++ main.tf
@@ -84,6 +84,12 @@ resource "aws_apigatewayv2_route" "eks_route_with_auth" {
 
 resource "aws_apigatewayv2_route" "docs_route" {
   api_id    = data.aws_apigatewayv2_api.api_gateway.id
-  route_key = "GET /docs"
+  route_key = "GET /docs/{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.eks_integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "favicon_route" {
+  api_id    = data.aws_apigatewayv2_api.api_gateway.id
+  route_key = "GET /favicon.ico"
   target    = "integrations/${aws_apigatewayv2_integration.eks_integration.id}"
 }
